e5913b9e25e936b7a682c81f75d93b3b8ca37280
PBillingsby <philbillingsby@gmail.com>
2022-10-10 08:51:45 -0400

    wip - 1

--- index.js
+++ index.js
@@ -5,30 +5,6 @@ const jwk = JSON.parse(fs.readFileSync("wallet.json").toString());
 import open from 'open';
 import { parseHTML } from './utils/parse.js'
 
-// const query = {
-//   query: `
-//   query {
-//     transactions(
-//         tags: {
-//             name: "Application",
-//             values: ["WikipediaArchiveTest"]
-//         }
-//     ) {
-//         edges {
-//             node {
-//                 id
-//                 tags {
-//                     name
-//                     value
-//                 }
-//             }
-//         }
-//     }
-//   }`
-// }
-
-const CLEANUP = ['src-id.txt', 'src-manifest.csv', 'src-manifest.json'];
-
 const getPage = async (query) => {
   let content
 
@@ -43,39 +19,84 @@ const getPage = async (query) => {
 const scrapePage = async (query) => {
   try {
     const content = await getPage(query)
-
-    const tags = [
-      { name: "Application", value: "WikiTestArchiver1" },
-      { name: "Page-ID", value: `${content.pageid ?? "unknown"}` },
-      { name: "Page-Title", value: content.title },
-      { name: "Content-Type", value: "text/html" }
-    ];
-
     const html = parseHTML(await content.html(), content.title);
 
-    createTransaction(html, tags)
+    prepareNft(html, content)
   }
   catch (err) {
     console.error(err)
   }
 }
 
-const createTransaction = async (page, tags) => {
+const prepareNft = async (page, content) => {
   const bundlr = new Bundlr.default("http://node2.bundlr.network", "arweave", jwk);
+  const tags = [
+    { name: "Application", value: "WikiTestArchiver1" },
+    { name: "Page-ID", value: `${content.pageid ?? "unknown"}` },
+    { name: "Page-Title", value: content.title },
+    { name: "Content-Type", value: "text/html" }
+  ];
   const tx = bundlr.createTransaction(page, { tags: tags })
+  try {
+    // await tx.sign();
+    // const res = await tx.upload();
 
-  await tx.sign();
+    createNFT(tx.id, content);
+    // await open(`https://arweave.net/${res.data.id}`)
 
-  const res = await tx.upload();
-  await open(`https://arweave.net/${res.data.id}`)
-
-  if (tx) {
-    CLEANUP.forEach(file => {
-      fs.unlink(file, function (err) {
-        if (err) throw err;
-      })
-    })
+    // if (tx) {
+    //   fs.unlink('src/index.html', function (err) {
+    //     if (err) throw err;
+    //   })
+    // }
+  } catch (err) {
+    console.log(err.message)
   }
+}
+
+const createNFT = async (tags, content) => {
+  console.log(content)
+  const initState = {
+    "title": `${content.title} Artefact`,
+    "name": `Artefact #${content.pageid}`,
+    "description": `Minted from archiving pool PLACEHOLDER`,
+    "ticker": "KOINFT",
+    "balances": {},
+    // "owner": tokenHolder,
+    "maxSupply": 1, // used to allow fractional shares without stepping into decimal territory
+    "contentType": "application/json",
+    "transferable": false,
+    "lockTime": 0,
+    "lastTransferTimestamp": null,
+    "createdAt": new Date().getTime()
+  }
+
+  // console.log(tags);
+  // console.log(initState);
+
+  // NFT CONTRACT ID: Qa7IR-xvPkBtcYUBZXd8z-Tu611VeJH33uEA5XiFUNA
+
+  // MANIFEST const manifest = {
+  //   manifest: "arweave/paths",
+  //   version: "0.1.0",
+  //   index: {},
+  //   paths: {}
+  // }
+
+  // returns tx id
+  // create contract with path manifest (router document pointing to tx id)
+  // returned contract id
+
+  //manifest.paths["index.html"] = { id: TX ID }
+  // manifest.index = { path: "index.html" }
+
+  // const tx = await warp.createContract.deployFromSourceTx({
+  //   srcTxId: NFTContractID,
+  //   wallet,
+  //   initState: JSON.stringify(initialState),
+  //   data: { "Content-Type": "application/x.arweave-manifest+json", body: JSON.stringify(manifest) },
+  //   tags
+  // })
 }
 
 scrapePage(process.argv[2])
--- utils/parse.js
+++ utils/parse.js
@@ -1,6 +1,5 @@
 import { fromHtml } from "hast-util-from-html"
 import { toHtml } from "hast-util-to-html"
-// import findAndReplace from 'hast-util-find-and-replace'
 import { map } from 'unist-util-map'
 import { h } from 'hastscript'
 
@@ -8,7 +7,6 @@ export const parseHTML = (content, title) => {
   const tree = fromHtml(content)
   const newTree = map(tree, node => {
     if (node.type === 'element' && node.tagName === 'head') {
-      //console.log(node)
       node.children =
         [
           h('link', { rel: 'stylesheet', href: 'https://arweave.net/ppG7r_LcsbQqyKaXUJp-VyNTStJciSibhxfRT73hT2I' }),
@@ -22,17 +20,9 @@ export const parseHTML = (content, title) => {
         node.properties.href = "https://wikipedia.org" + node.properties.href;
       }
     }
-    // if (node.type === 'element' && node.tagName === 'img') {
-    //   if (node?.properties?.href && node?.properties?.href.match("\^/wiki{0,}")) {
-    //     // node?.properties?.href = "https://wikipedia.org" + node?.properties.href;
-    //   }
-    //   return node
-    // }
+
     return node
   })
 
   return toHtml(newTree)
-  // const xhtml = toHtml(tree)
-  // save new html as an html file
-  // console.log(xhtml)
 }
